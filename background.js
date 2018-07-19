02f85b92f2f42b5d00cc3074d14965a4c82188b0
aakashdp <aakashdp@umich.edu>
2018-07-19 17:13:39 -0400

    choose device to connect to, open server-side connection

--- background.js
+++ background.js
@@ -6,19 +6,7 @@
  * Version 1.0
  * July 2018
  */
-
-// dictionary to hold information about devices
-var device_list = {};
-var add_device = function(device) {
-    device_list.push(device.address: device.name);
-};
-var remove_device = function(device) {
-    delete device_list[device.address];
-}
-
-// information about connecting to a device
-var uuid = '1105';
-var id = 0;
+ 
 var onConnectedCallback = function() {
     if (chrome.runtime.lastError) {
         console.log("Connection failed: " + chrome.runtime.lastError.message);
@@ -37,24 +25,12 @@ var stringToBuffer = function(str) {
     return buf;
 }
 
-// notify user when new device makes connection
-chrome.bluetooth.onDeviceAdded.addListener(function(device) {
-    console.log("New device found: " + device.address + " " + device.name);
-    add_device(device);
-});
-
-// notify when connection is lost
-chrome.bluetooth.onDeviceRemoved.addListener(function(device) {
-    console.log("Device disconnected: " + device.address + " " + device.name);
-    remove_device(device);
-});
-
 // start script on launch
 chrome.app.runtime.onLaunched.addListener(function() {
     // creates a window
     chrome.app.window.create('window.html', {
         'outerBounds': {
-            'width': 640,
+            'width': 360,
             'height': 480
         }
     });
@@ -63,47 +39,4 @@ chrome.app.runtime.onLaunched.addListener(function() {
     chrome.bluetooth.getAdapterState(function(adapter) {
         console.log("Adapter " + adapter.address + ": " + adapter.name);
     });
-
-    // find available devices
-    chrome.bluetooth.getDevices(function(devices) {
-        for (var i = 0; i < devices.length; i++) {
-            // log address and name
-            console.log(devices[i].address + ' ' + devices[i].name);
-            // add the discovered device to the list
-            add_device(devices[i]);
-        }
-    });
-
-    // print devices to window
-    for (var address in device_list) {
-        var device_id = "device" + address;
-        $("#devices-list").append("<div id=" + device_id + "><p>" +
-            device_list[i].name + "</p><p>" + device_list[i].address
-        )
-    }
-
-    var socket = -1;
-    // create socket
-    chrome.bluetoothSocket.create(function(createInfo) {
-        socket = createInfo.socketId;
-        try {
-            chrome.bluetoothSocket.connect(createInfo.socketId,
-                device_list[1]["address"], uuid, onConnectedCallback)
-            console.log("Connected to device " + device_list[0]["name"] + " on socket " + socket);
-
-            // try sending info to device
-            chrome.bluetoothSocket.send(socket, stringToBuffer("hello"), function(bytes_sent) {
-                if (chrome.runtime.lastError) {
-                    console.log("Send failed: " + chrome.runtime.lastError.message);
-                }
-                else {
-                    console.log("Sent " + bytes_sent + " bytes")
-                }
-            });
-        }
-        catch(error) {
-            console.log("error: " + error.message);
-        }
-    });
-
 });
--- scripts.js
+++ scripts.js
@@ -1,30 +1,156 @@
+// dictionary to hold information about devices
+// format = { address: device }
+var device_list = {};
+var device_id;
+
+// if connecting causes an error, print error
+var onConnectedCallback = function() {
+    if (chrome.runtime.lastError) {
+        console.log("Connection failed: " + chrome.runtime.lastError.message);
+    } else {
+        // do something else
+    }
+};
+
 function searchBluetoothDevices() {
-    console.log('Requesting Bluetooth Device...');
-    var options = {};
-    //options.acceptAllDevices = true;
-    options.filters = [{services: ['battery_service']}];
+    console.log('Requesting bluetooth devices...');
+    // find available devices
+    chrome.bluetooth.getDevices(function(devices) {
+        for (var i = 0; i < devices.length; i++) {
+            // add the discovered device to the list
+            device_list[devices[i].address] = devices[i];
+        }
+    });
+    console.log(Object.keys(device_list).length + " devices found");
+}
 
-    navigator.bluetooth.requestDevice(options)
-    .then(device => {
-        console.log("Connecting to GATT server...");
-        var $heading = $("<h1>Device found:</h1>");
+function displayDevices() {
+    console.log("Displaying devices...");
 
-        // get device info
-        var $name = $("<p>Name: " + device.name + "</p>");
-        var $id = $("<p>Id: " + device.id + "</p>");
-        var $connected = $("<p>Connected: " + device.gatt.connected + "</p>");
+    // clear the current list of devices
+    $("#devices-list").html("");
 
-        // print device info to screen
-        var $newDiv = $("<div id='device-info'></div>");
-        $newDiv.append($heading, $name, $id, $connected);
+    // add each device to list
+    for (var key in device_list) {
+        console.log(key + " " + device_list[key].name);
+        var $listElem = $("<li id='" + key + "'>" + device_list[key].name + "</li>");
+        $("#devices-list").append($listElem);
+    }
 
-        $("#device-info").replaceWith($newDiv);
-    })
+    // if >= 1 device is found, display the connect button
+    if (Object.keys(device_list).length > 0) {
+        $("#connect").show();
+    }
+}
+
+// Event handler for search button being pressed
+// Looks for bluetooth devices and displays them in the window
+function findAndDisplay(event) {
+    searchBluetoothDevices();
+    displayDevices();
+}
+
+// Event handler for a device being clicked in list
+// Changes colors of list elements to highlight clicked device and
+// saves the id of the clicked device for later use
+function deviceClicked(event) {
+    // change list colors
+    $("li").css("background-color", "white");
+    $(event.currentTarget).css("background-color", "#dddddd");
+
+    // save current device's id
+    device_id = event.currentTarget.id;
+    // console.log("Device " + device_id + " clicked");
+}
+
+// id is the address of the device to connect to
+function connectToDevice(id) {
+    // clear error
+    // if id is null, display error message
+    if (id == null) {
+        var errorMessage = "Error: please select a device before trying to connect.";
+        throwError("connection", errorMessage);
+        return;
+    }
+
+    clearErrors();
+
+    console.log("Connecting to device " + id);
+    console.log("Device uuids: " + device_list[id].uuids);
+
+    // this is the uuid used by the app
+    var uuid = "00001101-0000-1000-8000-00805f9b34fb";
+
+    var serverSocketId;
+    chrome.bluetoothSocket.create(function(createInfo) {
+        serverSocketId = createInfo.socketId;
+        chrome.bluetoothSocket.listenUsingRfcomm(serverSocketId,
+            uuid, onConnectedCallback);
+        console.log("Connected on socket " + serverSocketId);
+        $(".message").remove();
+        postMessage("Server-side connection established on socket " +
+            serverSocketId);
+    });
+
+
+    // add listener for what to do when connection comples
+    chrome.bluetoothSocket.onAccept.addListener(function(acceptInfo) {
+        if (acceptInfo.socketId != serverSocketId) {
+            return;
+        }
+
+        chrome.bluetoothSocket.send(acceptInfo.clientSocketId,
+            "hello", onSendCallback);
+
+        // Accepted sockets are initially paused,
+        // set the onReceive listener first.
+        chrome.bluetoothSocket.onReceive.addListener(onReceive);
+        chrome.bluetoothSocket.setPaused(false);
+    });
 
 }
 
-// when the button is clicked, look for bluetooth devices
+// type is an optional argument that indicates which error to remove
+function clearErrors(type) {
+    // if no type specified, clear all errors
+    if (type == null) {
+        $(".error").remove();
+    }
+    else {
+        $("#" + type + "-error").remove();
+    }
+}
+
+// type is where the error came from, ex. 'connection' or 'uuid'
+function throwError(type, message) {
+    $("#" + type + "-error").remove();
+    var $error = $("<div class='error' id='" + type + "-error'>" +
+                    "<p>" + message + "</p></div>");
+    $(document.body).append($error);
+}
+
+function postMessage(message) {
+    var $message = $("<div class='message'>" +
+                    "<p>" + message + "</p></div>");
+    $(document.body).append($message);
+}
+
+
+// These are the things that will happen when the window loads
+// Attach event handlers, hide/show elements, etc
 document.addEventListener('DOMContentLoaded', function() {
-    //document.getElementById("check-devices").onclick = myFunction;
-    $("#check-devices").on("click", searchBluetoothDevices);
+    $("#search").on("click", findAndDisplay);
+    // change color of li when clicked
+    $("#devices-list").on("click", "li", deviceClicked);
+
+    // hide the connect button but attach the event handler
+    // anonymous function used to encapsulate function call with argument
+    $("#connect").on("click", { "device_id": device_id }, function() {
+        connectToDevice(device_id);
+    });
+    $("#connect").hide();
+
+    // hide error message for now
+    $("#connection-error").hide();
+
 });
